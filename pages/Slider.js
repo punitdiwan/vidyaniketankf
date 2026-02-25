aba0909353e57822b2ba9e19a4b98fe03b5d1378
punitdiwan <punitdiwan@gmail.com>
2026-02-25 15:08:15 +0530

    update_sortorder_sliderbtn

--- pages/Slider.js
+++ pages/Slider.js
@@ -1,59 +1,74 @@
-
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-import { base_url, school_name } from '../SimpleState/auth'
-import axios from 'axios';
-import React, { useState, useEffect } from 'react';
-
-
+import React from "react";
+import { IoIosArrowForward ,IoIosArrowBack} from "react-icons/io";
 const Slider = ({ slider_data }) => {
 
-
-
   const slides = [
-    { title: "/images/is15.jpg ", description: 'Lorem ipsum' },
-    { title: "/images/sd2.jpg", description: 'Lorem ipsum' },
+    { title: "/images/i dess15.jpg",cription: "Lorem ipsum" },
+    { title: "/images/sd2.jpg", description: "Lorem ipsum" },
   ];
   const sortedSlides = slider_data?.data
     ? [...slider_data.data].sort((a, b) => a.sort_order - b.sort_order)
     : [];
-    console.log("slider_data",slider_data)
-
-
   return (
-    <div>
-      <Carousel showThumbs={false} autoPlay={true} infiniteLoop={true} className="mt-14" >
-        {sortedSlides.length > 0 ?
-          sortedSlides.map((item, index) => {
-            return <div className="carousel-inner" role="listbox" key={index} >
-              <div className='carousel' role="listbox">
+    <div className="relative">
+      <Carousel
+        showThumbs={false}
+        autoPlay={true}
+        infiniteLoop={true}
+        interval={3000}
+        showStatus={false}
+        className="mt-14"
+        
+        renderArrowPrev={(onClickHandler, hasPrev, label) =>
+          hasPrev && (
+            <button
+              type="button"
+              onClick={onClickHandler}
+              title={label}
+              className="absolute left-4 top-1/2 -translate-y-1/2 z-10 bg-white/20 text-white p-3 rounded-full hover:bg-white hover:text-black transition"
+            >
+              <IoIosArrowBack size={26}/>
+            </button>
+          )
+        }
+
+        renderArrowNext={(onClickHandler, hasNext, label) =>
+          hasNext && (
+            <button
+              type="button"
+              onClick={onClickHandler}
+              title={label}
+              className="absolute right-4 top-1/2 -translate-y-1/2 z-10 bg-white/20 text-white p-3 rounded-full hover:bg-white hover:text-black transition"
+            >
+              <IoIosArrowForward size={26}/>
+            </button>
+          )
+        }
+      >
+        {sortedSlides.length > 0
+          ? sortedSlides.map((item, index) => (
+              <div key={index}>
                 <img
-                  src={item?.image?.data?.full_url?.replace('http://', 'https://')
-                  }
-                  className="w-full md:h-[400px] lg:h-[500px] h-[230px]"
-                  alt="sorry_no_img"
+                  src={item?.image?.data?.full_url?.replace("http://", "https://")}
+                  className="w-full md:h-[400px] lg:h-[500px] h-[230px] object-cover"
+                  alt="slider_img"
                 />
               </div>
-            </div>
-          })
-          :
-          slides.map((slide, index) => {
-            return <div className="carousel-inner" key={index} role="listbox">
-              <div className='carousel' role="listbox">
+            ))
+          : slides.map((slide, index) => (
+              <div key={index}>
                 <img
                   src={slide.title}
-                  height={500} width={1500}
-                  alt="sorry_no_img"
-                  className="w-full md:h-[400px] lg:h-[500px]"
+                  alt="slider_img"
+                  className="w-full md:h-[400px] lg:h-[500px] h-[230px] object-cover"
                 />
               </div>
-            </div>
-          })}
+            ))}
       </Carousel>
     </div>
-  )
-}
+  );
+};
 
-export default Slider
-
-
+export default Slider;
